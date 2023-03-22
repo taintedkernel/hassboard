81d459b12bc3a4b0264869741d7faa4dbd7bb3af
Anthony DeChiaro <root@smartgirder>
2023-03-22 12:47:07 -0700

    Add logging features

--- src/include/logger.h
+++ src/include/logger.h
@@ -2,6 +2,7 @@
 #define LOGGER_H
 
 #include <stdarg.h>
+#include <string>
 
 #define TERM_RED      "\033[1;31m"
 #define TERM_YELLOW   "\033[1;33m"
@@ -11,12 +12,20 @@
 #define LOG_TO_FILE   true
 #define LOG_PATH      "/dev/shm/smartgirder.log"
 
+#define __METHOD__          methodName(__PRETTY_FUNCTION__, "")
+#define __METHOD_ARG__(arg) methodName(__PRETTY_FUNCTION__, arg)
+
 void initLogger(void);
 void shutdownLogger(void);
 void _error(const char *fmt, ...);
+void _error(std::string fmt, ...);
 void _warn(const char *fmt, ...);
+void _warn(std::string fmt, ...);
 void _debug(const char *fmt, ...);
+void _debug(std::string fmt, ...);
 void _log(const char *fmt, ...);
+void _log(std::string fmt, ...);
 void __logHelper(const char *header, const char *fmt, va_list argptr);
+std::string methodName(const std::string& prettyFunction, std::string arg);
 
 #endif
--- src/logger.cpp
+++ src/logger.cpp
@@ -28,11 +28,27 @@ void _error(const char *fmt, ...)
   va_end(argptr);
 }
 
+void _error(std::string fmt, ...)
+{
+  va_list argptr;
+  va_start(argptr, fmt);
+  _error(fmt.c_str(), argptr);
+  va_end(argptr);
+}
+
 void _warn(const char *fmt, ...)
 {
   va_list argptr;
   va_start(argptr, fmt);
   __logHelper(TERM_YELLOW "[warn] ", fmt, argptr);
+  va_end(argptr);
+}
+
+void _warn(std::string fmt, ...)
+{
+  va_list argptr;
+  va_start(argptr, fmt);
+  _warn(fmt.c_str(), argptr);
   va_end(argptr);
 }
 
@@ -44,11 +60,27 @@ void _debug(const char *fmt, ...)
   va_end(argptr);
 }
 
+void _debug(std::string fmt, ...)
+{
+  va_list argptr;
+  va_start(argptr, fmt);
+  _debug(fmt.c_str(), argptr);
+  va_end(argptr);
+}
+
 void _log(const char *fmt, ...)
 {
   va_list argptr;
   va_start(argptr, fmt);
   __logHelper(TERM_BLUE "[info] ", fmt, argptr);
+  va_end(argptr);
+}
+
+void _log(std::string fmt, ...)
+{
+  va_list argptr;
+  va_start(argptr, fmt);
+  _log(fmt.c_str(), argptr);
   va_end(argptr);
 }
 
@@ -65,4 +97,14 @@ void __logHelper(const char *header, const char *fmt, va_list argptr)
     fprintf(logger, "\n");
     fflush(logger);
   }
+}
+
+// https://stackoverflow.com/questions/1666802/is-there-a-class-macro-in-c
+std::string methodName(const std::string& prettyFunction, std::string arg)
+{
+    size_t colons = prettyFunction.find("::");
+    size_t begin = prettyFunction.substr(0, colons).rfind(" ") + 1;
+    size_t end = prettyFunction.rfind("(") - begin;
+
+    return prettyFunction.substr(begin, end) + "(" + arg + ")";
 }
